93fcd71a5f958541e1e2e8ca7183f8fc0659291d
DreamFright <dimuronathan@protonmail.com>
2024-12-10 11:09:38 +0100

    10/12/24 2
    added endpoint implementation for genres

--- vosgespedia-api/app/Http/Controllers/GenreController.php
+++ vosgespedia-api/app/Http/Controllers/GenreController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Genre;
 
 class GenreController extends Controller
 {
-    //
+    public function listGenres(Request $request) {
+        $response = null;
+        if (isset($request->name)) {
+            $response = Genre::where('genre_name','like','%' . $request->name . '%')->get();
+        }
+        elseif (isset($request->id)) {
+            $response = Genre::where('gid','=',$request->id)->get();
+        }
+        else {
+            $response = Genre::all();
+        }
+        return response()->json($response);
+    }
 }
--- vosgespedia-api/routes/api.php
+++ vosgespedia-api/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
 use App\Http\Controllers\TraceController;
 use App\Http\Controllers\CategorieController;
+use App\Http\Controllers\GenreController;
 
 Route::get('/animals', [AnimalController::class, 'listAnimal']);
 Route::get('/animtrace', [AnimalController::class, 'getTraceFromId']);
@@ -11,5 +12,6 @@ Route::get('/traces', [TraceController::class, 'listTrace']);
 Route::get('/traceanim', [TraceController::class, 'getAnimalFromId']);
 Route::get('/cattr', [CategorieController::class, 'getTracesFromCat']);
 Route::get('/categories', [CategorieController::class, 'listCategories']);
+Route::get('/categories', [GenreController::class, 'listGenres']);
 
 ?>

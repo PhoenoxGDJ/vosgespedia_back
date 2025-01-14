77a9c60610a7c45161469c0419b9bc7f0e741e81
DreamFright <dimuronathan@protonmail.com>
2025-01-14 17:24:56 +0100

    Update CorsHeader.php

--- vosgespedia-api/app/Http/Middleware/CorsHeader.php
+++ vosgespedia-api/app/Http/Middleware/CorsHeader.php
@@ -20,6 +20,14 @@ class CorsHeader
         $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
         $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
 
+        if ($request->getMethod() === "OPTIONS") {
+            return response()->json('OK', 200, [
+                'Access-Control-Allow-Origin' => '*',
+                'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS',
+                'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
+            ]);
+        }
+
         return $response;
     }
 }

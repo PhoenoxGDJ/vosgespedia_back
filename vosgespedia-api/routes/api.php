94d85550a5b222e07671010d2f24e62f950fc68f
DreamFright <dimuronathan@protonmail.com>
2025-01-14 14:53:29 +0100

    wtf

--- vosgespedia-api/routes/api.php
+++ vosgespedia-api/routes/api.php
@@ -5,23 +5,23 @@ use App\Http\Controllers\AnimalController;
 use App\Http\Controllers\TraceController;
 use App\Http\Controllers\CategorieController;
 use App\Http\Controllers\GenreController;
-use App\Http\Middleware\CorsHeader;
+use Illuminate\Http\Middleware\HandleCors;
 
-Route::get('/animals', [AnimalController::class, 'listAnimal'])->middleware(CorsHeader::class);
-Route::get('/animals/{arg}', [AnimalController::class, 'listAnimal'])->middleware(CorsHeader::class);
+Route::get('/animals', [AnimalController::class, 'listAnimal'])->middleware(HandleCors::class);
+Route::get('/animals/{arg}', [AnimalController::class, 'listAnimal'])->middleware(HandleCors::class);
 
-Route::get('/animtrace', [AnimalController::class, 'getTraceFromId'])->middleware(CorsHeader::class);
+Route::get('/animtrace', [AnimalController::class, 'getTraceFromId'])->middleware(HandleCors::class);
 
-Route::get('/traces', [TraceController::class, 'listTrace'])->middleware(CorsHeader::class);
-Route::get('/traces/{arg}', [TraceController::class, 'listTrace'])->middleware(CorsHeader::class);
+Route::get('/traces', [TraceController::class, 'listTrace'])->middleware(HandleCors::class);
+Route::get('/traces/{arg}', [TraceController::class, 'listTrace'])->middleware(HandleCors::class);
 
-Route::get('/traceanim', [TraceController::class, 'getAnimalFromId'])->middleware(CorsHeader::class);
+Route::get('/traceanim', [TraceController::class, 'getAnimalFromId'])->middleware(HandleCors::class);
 
-Route::get('/cattr/{arg}', [CategorieController::class, 'getTracesFromCat'])->middleware(CorsHeader::class);
-Route::get('/categories', [CategorieController::class, 'listCategories'])->middleware(CorsHeader::class);
-Route::get('/categories/{arg}', [CategorieController::class, 'listCategories'])->middleware(CorsHeader::class);
+Route::get('/cattr/{arg}', [CategorieController::class, 'getTracesFromCat'])->middleware(HandleCors::class);
+Route::get('/categories', [CategorieController::class, 'listCategories'])->middleware(HandleCors::class);
+Route::get('/categories/{arg}', [CategorieController::class, 'listCategories'])->middleware(HandleCors::class);
 
-Route::get('/genres', [GenreController::class, 'listGenres'])->middleware(CorsHeader::class);
-Route::get('/genres/{arg}', [GenreController::class, 'listGenres'])->middleware(CorsHeader::class);
+Route::get('/genres', [GenreController::class, 'listGenres'])->middleware(HandleCors::class);
+Route::get('/genres/{arg}', [GenreController::class, 'listGenres'])->middleware(HandleCors::class);
 
 ?>
